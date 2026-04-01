1ad7aa1a58adbc819d0d79daacd369c8cf4bf603
Amplify1-acct <accounts@amplifylaw.ai>
2026-04-01 06:45:20 -0400

    feat: add blog/social preview to step 3

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ const nextConfig = {
       "images.pexels.com",
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/homepage-v4-light-indigo.html",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = withSentryConfig(nextConfig, {
